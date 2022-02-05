c18721550a9c04d98bba779ebc8d491323b3bc35
srijarkoroy <sr8962@srmist.edu.in>
2022-02-06 00:44:21 +0530

    algorithms module initialization

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,1 +1,5 @@
-
+from .classification import Classification
+from .face_detection import FaceDetection
+from .objectDetection import ObjectDetection
+from .instance_segmentation import InstanceSegmentation
+from .semantic_segmentation import SemanticSegmentation
